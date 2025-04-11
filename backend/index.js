7214cacf64d3b2e14a382e110fe362df270f7654
Shivank Dixit <shivankdixit@Shivanks-MacBook-Air.local>
2025-04-11 14:23:08 +0530

    pushed

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,30 @@
 const express = require('express')
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 const cors = require('cors')
 const authRoute = require('./routes/auth-route')
 const adminRoute = require('./routes/admin-route')
-const { connectToDB } = require('./db/db');
-dotenv.config();
+const { connectToDB } = require('./db/db'); 
+dotenv.config(); 
+const path = require('path')
 
 app.get('/', (req, res)=>{
     return res.send("basic setup")
 })
 
 const corsOptions = {
-    method: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     origin: `${process.env.FRONTEND_URL}` || '*',
     credentials: true
-}
+}  
+
 
 app.use(cors(corsOptions))
 app.use(express.json())
 
 app.use('/api/user', authRoute)
-app.use('/api/admin', adminRoute) 
+app.use('/api/admin', adminRoute)  
+
 app.listen(process.env.PORT, (req, res)=>{
     console.log('Listening on PORT ', process.env.PORT)
 })
